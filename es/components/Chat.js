540031f293ab37b9aa6b818039849a4250a330be
Julien Barret <julien.barret@webcastor.fr>
2019-03-11 16:05:14 +0100

    Added build

---
+++ es/components/Chat.js
@@ -0,0 +1,85 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import ChatWindow from "./ChatWindow";
+import launcherIcon from "./../assets/logo-no-bg.svg";
+import incomingMessageSound from "./../assets/sounds/notification.mp3";
+
+var Chat = function (_Component) {
+  _inherits(Chat, _Component);
+
+  function Chat() {
+    _classCallCheck(this, Chat);
+
+    var _this = _possibleConstructorReturn(this, _Component.call(this));
+
+    _this.state = {
+      launcherIcon: launcherIcon,
+      isOpen: false
+    };
+    return _this;
+  }
+
+  Chat.prototype.componentWillReceiveProps = function componentWillReceiveProps(nextProps) {
+    if (this.props.mute) {
+      return;
+    }
+    var nextMessage = nextProps.messageList[nextProps.messageList.length - 1];
+    var isIncoming = (nextMessage || {}).author === "them";
+    var isNew = nextProps.messageList.length > this.props.messageList.length;
+    if (isIncoming && isNew) {
+      this.playIncomingMessageSound();
+    }
+  };
+
+  Chat.prototype.playIncomingMessageSound = function playIncomingMessageSound() {
+    var audio = new Audio(incomingMessageSound);
+    audio.play();
+  };
+
+  Chat.prototype.handleClick = function handleClick() {
+    if (this.props.handleClick !== undefined) {
+      this.props.handleClick();
+    } else {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+  };
+
+  Chat.prototype.render = function render() {
+    return React.createElement(ChatWindow, {
+      messageList: this.props.messageList,
+      onUserInputSubmit: this.props.onMessageWasSent,
+      onFilesSelected: this.props.onFilesSelected,
+      agentProfile: this.props.agentProfile,
+      onClose: this.handleClick.bind(this),
+      showEmoji: this.props.showEmoji
+    });
+  };
+
+  return Chat;
+}(Component);
+
+Chat.propTypes = process.env.NODE_ENV !== "production" ? {
+  onMessageWasReceived: PropTypes.func,
+  onMessageWasSent: PropTypes.func,
+  newMessagesCount: PropTypes.number,
+  isOpen: PropTypes.bool,
+  handleClick: PropTypes.func,
+  messageList: PropTypes.arrayOf(PropTypes.object),
+  mute: PropTypes.bool,
+  showEmoji: PropTypes.bool
+} : {};
+
+Chat.defaultProps = {
+  newMessagesCount: 0,
+  showEmoji: true
+};
+
+export default Chat;
---
+++ es/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import './styles';
+import Chat from './components/Chat';
+
+export { Chat };
